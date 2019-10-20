23fb91990b20c5cc93d0c9a1bf7dbf4b5fcc7480
Nikhilverma92 <56505588+Nikhilverma92@users.noreply.github.com>
2019-10-20 16:22:53 +0530

    Update actions.py

--- actions.py
+++ actions.py
@@ -6,6 +6,11 @@ from rasa_core.actions.action import Action
 from rasa_core.events import SlotSet
 import zomatopy
 import json
+import pandas as pd
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 class Searchforlocation(Action):
@@ -15,8 +20,8 @@ class Searchforlocation(Action):
     def run(self, dispatcher, tracker, domain):
         locn = tracker.get_slot('location')
         dispatcher.utter_message("------------------------------\n"+locn)
-        FromTier12 = ['pune','delhi','mumbai','noida']
-        FromTier3 = ['bhutan','gurgaon']
+        FromTier12 = ['bangalore', 'chennai', 'delhi', 'hyderabad', 'kolkata', 'mumbai', 'ahmedabad', 'pune', 'agra', 'ajmer', 'aligarh', 'amravati', 'amritsar', 'asansol', 'aurangabad', 'bareilly', 'belgaum', 'bhavnagar', 'bhiwandi', 'bhopal', 'bhubaneswar', 'bikaner', 'bilaspur', 'bokaro steel city', 'chandigarh', 'coimbatore nagpur', 'cuttack', 'dehradun', 'dhanbad', 'bhilai', 'durgapur', 'erode', 'faridabad', 'firozabad', 'ghaziabad', 'gorakhpur', 'gulbarga', 'guntur', 'gwalior', 'gurgaon', 'guwahati', 'hubli–dharwad', 'indore', 'jabalpur', 'jaipur', 'jalandhar', 'jammu', 'jamnagar', 'jamshedpur', 'jhansi', 'jodhpur', 'kakinada', 'kannur', 'kanpur', 'kochi', 'kottayam', 'kolhapur', 'kollam', 'kota', 'kozhikode', 'kurnool', 'ludhiana', 'lucknow', 'madurai', 'malappuram', 'mathura', 'goa', 'mangalore', 'meerut', 'moradabad', 'mysore', 'nanded', 'nashik', 'nellore', 'noida', 'palakkad', 'patna', 'pondicherry', 'purulia allahabad', 'raipur', 'rajkot', 'rajahmundry', 'ranchi', 'rourkela', 'salem', 'sangli', 'siliguri', 'solapur', 'srinagar', 'thiruvananthapuram', 'thrissur', 'tiruchirappalli', 'tirupati', 'tirunelveli', 'tiruppur', 'tiruvannamalai', 'ujjain', 'bijapur', 'vadodara', 'varanasi', 'vasai-virar city', 'vijayawada', 'vellore', 'warangal', 'surat', 'visakhapatnam']
+        FromTier3 = ['bhutan','Mexico']
         if locn.lower() in FromTier12:
             responses = "Tier12"
             dispatcher.utter_message("---------------It is in Tier12---------------\n")    
@@ -33,28 +38,101 @@ class Searchforlocation(Action):
         
 
 class ActionSearchRestaurants(Action):
+    def name(self):
+        return 'action_restaurant'
+
+    def run(self, dispatcher, tracker, domain):
+        config={ "user_key":"5200b060757d691783e5c806775b36bd"}
+        zomato = zomatopy.initialize_app(config)
+        loc = tracker.get_slot('location')
+        cuisine = tracker.get_slot('cuisine')
+        price = tracker.get_slot('price')
+        location_detail=zomato.get_location(loc, 1)
+        d1 = json.loads(location_detail)
+        lat=d1["location_suggestions"][0]["latitude"]
+        lon=d1["location_suggestions"][0]["longitude"]
+        cuisines_dict={'mexican':73,'American':1,'chinese':25,'italian':55,'north indian':50,'south indian':85}
+        results=zomato.restaurant_search_all("", lat, lon, str(cuisines_dict.get(cuisine)),100)
+        d = json.loads(results)
+        response=""
+        output_data=""
+        restaurant_data={}
+        if d['results_found'] == 0:
+            output_data= "No Restaurants were found for the given input."
+        else:
+            """
+            for restaurant in d['restaurants']:
+                #response=response+ restaurant['restaurant']['name']+ " in "+ restaurant['restaurant']['location']['address']+ " has been rated ---" + restaurant['restaurant']['user_rating']['aggregate_rating'] + "\n\n"
+               
+                restaurant_data['Name']=restaurant['restaurant']['name'];
+                restaurant_data['Location']=restaurant['restaurant']['location']['address'];
+                restaurant_data['Rating']=restaurant['restaurant']['user_rating']['aggregate_rating'];
+                restaurant_data['CFT']=restaurant['restaurant']['average_cost_for_two'];
+            """   
+            response = pd.DataFrame({"Name":restaurant['restaurant']['name'],"Location":restaurant['restaurant']['location']['address'],"Rating":restaurant['restaurant']['user_rating']['aggregate_rating'],"CFT":restaurant['restaurant']['average_cost_for_two']} for restaurant in d['restaurants'])
+
+            if price.lower() == "low":
+                response = response[response['CFT']<=300];
+            elif price.lower() == "medium":
+                response = response[(response['CFT']>300) & (response['CFT']<=700)]
+            else:
+                response = response[response['CFT']>700]
+
+            #response = response.sort_values(by=["CFT"],ascending =False)
+
+            response=response.head(5);
+            #dispatcher.utter_message("---------------------------------------------\n"response)
+                        
+            for a in response.index:
+                output_data= output_data + str(response["Name"][a]) + " in " + str(response["Location"][a]) + " has been rated -- " + str(response["Rating"][a]) +" CFT is --  "+ str(response["CFT"][a]) + "\n"
+
+            dispatcher.utter_message("------------------------------\n"+output_data)
+        return [SlotSet('location',loc)]
+
+	
+class send_mail(Action):
 	def name(self):
-		return 'action_restaurant'
-
+			return 'action_email'
+		
 	def run(self, dispatcher, tracker, domain):
-		config={ "user_key":"5200b060757d691783e5c806775b36bd"}
-		zomato = zomatopy.initialize_app(config)
-		loc = tracker.get_slot('location')
-		cuisine = tracker.get_slot('cuisine')
-        #price = tracker.get_slot('price')
-		location_detail=zomato.get_location(loc, 1)
-		d1 = json.loads(location_detail)
-		lat=d1["location_suggestions"][0]["latitude"]
-		lon=d1["location_suggestions"][0]["longitude"]
-		cuisines_dict={'mexican':73,'American':1,'chinese':25,'italian':55,'north indian':50,'south indian':85}
-		results=zomato.restaurant_search("", lat, lon, str(cuisines_dict.get(cuisine)), 5)
-		d = json.loads(results)
-		response=""
-		if d['results_found'] == 0:
-			response= "no results"
-		else:
-			for restaurant in d['restaurants']:
-				response=response+ restaurant['restaurant']['name']+ " in "+ restaurant['restaurant']['location']['address']+ " has been rated ---" + restaurant['restaurant']['user_rating']['aggregate_rating'] + "\n\n"
+		#email_received = tracker.get_slot ('email')
+		#restaurants10 = restaurants.head(10)
+		# creates SMTP session 
+		s = smtplib.SMTP('smtp.gmail.com', 587) 
+  
+		# start TLS for security 
+		s.starttls() 
+  
+		# Authentication 
+		s.login("restaurantfoodiesearch@gmail.com", "qwerty@123") 
+  
+		# message to be sent 
+		message = "This is test mail"
+ 
+		# sending the mail 
+		s.sendmail("restaurantfoodiesearch@gmail.com", "akkic2@gmail.com", message) 
+  
+		# terminating the session 
+		s.quit()
+		print("Mail sent")
 
-		dispatcher.utter_message("------------------------------\n"+response)
-		return [SlotSet('location',loc)]
+ 		mail_content = 'Hello, This is a simple mail.'
+		#The mail addresses and password
+		sender_address = 'restaurantfoodiesearch@gmail.com'
+		sender_pass = 'qwerty@123'
+		receiver_address = 'akkic2@gmail.com'
+		#Setup the MIME
+		message = MIMEMultipart()
+		message['From'] = sender_address
+		message['To'] = receiver_address
+		message['Subject'] = 'A test mail sent by Python. It has not an attachment.'   #The subject line
+		#The body and the attachments for the mail
+		message.attach(MIMEText(mail_content, 'plain'))
+		#Create SMTP session for sending the mail
+		session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+		session.starttls() #enable security
+		session.login(sender_address, sender_pass) #login with mail_id and password
+		text = message.as_string()
+		session.sendmail(sender_address, receiver_address, text)
+		session.quit()
+		print('Mail Sent')
