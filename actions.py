3d33c0f4218919d1bf18a9c20db3037f95b2e36e
Nikhilverma92 <56505588+Nikhilverma92@users.noreply.github.com>
2019-10-20 16:41:37 +0530

    Update actions.py

--- actions.py
+++ actions.py
@@ -115,24 +115,3 @@ class send_mail(Action):
 		# terminating the session 
 		s.quit()
 		print("Mail sent")
-
- 		#mail_content = 'Hello, This is a simple mail.'
-		#The mail addresses and password
-		#sender_address = 'restaurantfoodiesearch@gmail.com'
-		#sender_pass = 'qwerty@123'
-		#receiver_address = 'akkic2@gmail.com'
-		#Setup the MIME
-		#message = MIMEMultipart()
-		#message['From'] = sender_address
-		#message['To'] = receiver_address
-		#message['Subject'] = 'A test mail sent by Python. It has not an attachment.'   #The subject line
-		#The body and the attachments for the mail
-		#message.attach(MIMEText(mail_content, 'plain'))
-		#Create SMTP session for sending the mail
-		#session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
-		#session.starttls() #enable security
-		##session.login(sender_address, sender_pass) #login with mail_id and password
-		#text = message.as_string()
-		#session.sendmail(sender_address, receiver_address, text)
-		#session.quit()
-		#print('Mail Sent')
