3ad11f3c6f0a3233855f7a8484e1e61db64a4313
Nikhilverma92 <56505588+Nikhilverma92@users.noreply.github.com>
2019-10-20 16:23:41 +0530

    Update zomatopy.py

--- zomatopy.py
+++ zomatopy.py
@@ -229,6 +229,19 @@ class Zomato:
         return r#a = ast.literal_eval(r)
 
 
+    def restaurant_search_all(self, query="", latitude="", longitude="", cuisines="", limit=100):
+        """
+        Takes either query, latitude and longitude or cuisine as input.
+        Returns a list of Restaurant IDs.
+        """
+        cuisines = "%2C".join(cuisines.split(","))
+        if str(limit).isalpha() == True:
+            raise ValueError('LimitNotInteger')
+        headers = {'Accept': 'application/json', 'user-key': self.user_key}
+        r = (requests.get(base_url + "search?q=" + str(query) + "&count=" + str(limit) + "&lat=" + str(latitude) + "&lon=" + str(longitude) + "&cuisines=" + str(cuisines) + "&sort=rating&order=desc" , headers=headers).content).decode("utf-8")
+        return r#a = ast.literal_eval(r)
+        
+        
     def get_location(self, query="", limit=5):
         """
         Takes either query, latitude and longitude or cuisine as input.
